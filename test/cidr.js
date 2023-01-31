1845ced317b50c76136f88f6dbd38ce651128b50
Andrea Bonomi <andrea.bonomi@gmail.com>
2023-01-31 11:49:36 +0100

    tests

--- test/cidr.js
+++ test/cidr.js
@@ -1,62 +1,76 @@
-var assert = require('assert');
-var iplib = require('../iplib');
+const assert = require('assert');
+const iplib = require('../iplib');
 
 describe('CIDR', function() {
 
     it('constructor and equal', function() {
-        var c = new iplib.CIDR("192.168.0.0/24");
-        var d = new iplib.CIDR("192.168.0.0/255.255.255.0");
-        var e = new iplib.CIDR("192.79.0.0/32");
-        var f = new iplib.CIDR("192.168.0.0/11111111111111111111111100000000");
+        const c = new iplib.CIDR("192.168.0.0/24");
+        const d = new iplib.CIDR("192.168.0.0/255.255.255.0");
+        const e = new iplib.CIDR("192.79.0.0/32");
+        const f = new iplib.CIDR("192.168.0.0/11111111111111111111111100000000");
         assert.ok(c.equals(d));
         assert.ok(d.equals(c));
         assert.ok(!d.equals(e));
         assert.ok(d.equals(f));
-        c = new iplib.CIDR("1.2.3.4/24");
-        d = new iplib.CIDR("16909060/255.255.255.0");
-        e = new iplib.CIDR("00000001000000100000001100000100/24");
-        f = new iplib.CIDR("00000001000000100000001100000100/11111111111111111111111100000000");
-        assert.ok(c.equals(d));
-        assert.ok(d.equals(c));
-        assert.ok(d.equals(e));
-        assert.ok(d.equals(f));
+        const g = new iplib.CIDR("1.2.3.4/24");
+        const h = new iplib.CIDR("16909060/255.255.255.0");
+        const i = new iplib.CIDR("00000001000000100000001100000100/24");
+        const j = new iplib.CIDR("00000001000000100000001100000100/11111111111111111111111100000000");
+        assert.ok(g.equals(h));
+        assert.ok(h.equals(g));
+        assert.ok(h.equals(i));
+        assert.ok(h.equals(j));
     });
 
     it('get first ip', function() {
-        var c = new iplib.CIDR("192.168.0.0/24");
-        var i = new iplib.IPv4Address("192.168.0.1");
+        const c = new iplib.CIDR("192.168.0.0/24");
+        const i = new iplib.IPv4Address("192.168.0.1");
         assert.ok(c.getFirstIp().equals(i));
-        c = new iplib.CIDR("192.168.0.79/32");
-        i = new iplib.IPv4Address("192.168.0.79");
-        assert.ok(c.getFirstIp().equals(i));
-        c = new iplib.CIDR("192.168.0.0/31");
-        i = new iplib.IPv4Address("192.168.0.0");
-        assert.ok(c.getFirstIp().equals(i));
+        const d = new iplib.CIDR("192.168.0.79/32");
+        const j = new iplib.IPv4Address("192.168.0.79");
+        assert.ok(d.getFirstIp().equals(j));
+        const e = new iplib.CIDR("192.168.0.0/31");
+        const k = new iplib.IPv4Address("192.168.0.0");
+        assert.ok(e.getFirstIp().equals(k));
     });
 
     it('get last ip', function() {
-        var c = new iplib.CIDR("192.168.0.0/24");
-        var i = new iplib.IPv4Address("192.168.0.254");
+        const c = new iplib.CIDR("192.168.0.0/24");
+        const i = new iplib.IPv4Address("192.168.0.254");
         assert.ok(c.getLastIp().equals(i));
-        c = new iplib.CIDR("192.168.0.79/32");
-        i = new iplib.IPv4Address("192.168.0.79");
-        assert.ok(c.getLastIp().equals(i));
-        c = new iplib.CIDR("192.168.0.0/31");
-        i = new iplib.IPv4Address("192.168.0.1");
-        assert.ok(c.getLastIp().equals(i));
+        const d = new iplib.CIDR("192.168.0.79/32");
+        const j = new iplib.IPv4Address("192.168.0.79");
+        assert.ok(d.getLastIp().equals(j));
+        const e = new iplib.CIDR("192.168.0.0/31");
+        const k = new iplib.IPv4Address("192.168.0.1");
+        assert.ok(e.getLastIp().equals(k));
     });
 
     it('get ip number', function() {
-        var c = new iplib.CIDR("192.168.0.0/24");
+        const c = new iplib.CIDR("192.168.0.0/24");
         assert.equal(c.getIpNumber(), 254);
-        c = new iplib.CIDR("1.2.3.4/8");
-        assert.equal(c.getIpNumber(), 16777214);
-        c = new iplib.CIDR("1.2.3.4/32");
-        assert.equal(c.getIpNumber(), 1);
-        c = new iplib.CIDR("1.2.3.4/31");
-        assert.equal(c.getIpNumber(), 2);
-        c = new iplib.CIDR("1.2.3.4/0");
-        assert.equal(c.getIpNumber(), 4294967294);
+        const d = new iplib.CIDR("1.2.3.4/8");
+        assert.equal(d.getIpNumber(), 16777214);
+        const e = new iplib.CIDR("1.2.3.4/32");
+        assert.equal(e.getIpNumber(), 1);
+        const f = new iplib.CIDR("1.2.3.4/31");
+        assert.equal(f.getIpNumber(), 2);
+        const g = new iplib.CIDR("1.2.3.4/0");
+        assert.equal(g.getIpNumber(), 4294967294);
+    });
+
+    it('get all valid ip', function() {
+        const c = new iplib.CIDR("192.168.0.0/24");
+        let count = 0;
+        for (const ip of c.getAllValidIp()) {
+            if (count == 0) {
+                assert.ok(c.getFirstIp().equals(ip));
+            } else if (count == c.getIpNumber() - 1) {
+                assert.ok(c.getLastIp().equals(ip));
+            }
+            count++;
+        }
+        assert.equal(c.getIpNumber(), count);
     });
 
 });
--- test/ipv4_address.js
+++ test/ipv4_address.js
@@ -1,25 +1,29 @@
-var assert = require('assert');
-var iplib = require('../iplib');
+const assert = require('assert');
+const iplib = require('../iplib');
 
 describe('IPv4Address', function() {
     it('constructor and equal', function() {
         assert.equal(-1, [1,2,3].indexOf(4));
-        var c = new iplib.IPv4Address("1.2.3.4");
+        const c = new iplib.IPv4Address("1.2.3.4");
         assert.equal(c.getDec(), 16909060);
         assert.equal(c.getBin(), "00000001000000100000001100000100");
         assert.equal(c.getDot(), "1.2.3.4");
-        var d = new iplib.IPv4Address("1.2.3.4", iplib.IP_DOT);
-        var e = new iplib.IPv4Address(16909060, iplib.IP_DEC);
-        var f = new iplib.IPv4Address("00000001000000100000001100000100", iplib.IP_BIN);
+        const d = new iplib.IPv4Address("1.2.3.4", iplib.IP_DOT);
+        const e = new iplib.IPv4Address(16909060, iplib.IP_DEC);
+        const f = new iplib.IPv4Address("00000001000000100000001100000100", iplib.IP_BIN);
         assert.ok(c.equals(d));
         assert.ok(c.equals(e));
         assert.ok(c.equals(f));
-        c = new iplib.IPv4Address("1.2.3.4");
-        d = new iplib.IPv4Address("1.2.3.4");
-        e = new iplib.IPv4Address("1.2.3.5");
-        assert.ok(c.equals(d));
-        assert.ok(d.equals(c));
-        assert.ok(!e.equals(d));
-        assert.ok(!d.equals(e));
+        const g = new iplib.IPv4Address("1.2.3.4");
+        const h = new iplib.IPv4Address("1.2.3.4");
+        const i = new iplib.IPv4Address("1.2.3.5");
+        assert.ok(g.equals(h));
+        assert.ok(h.equals(g));
+        assert.ok(!i.equals(h));
+        assert.ok(!h.equals(i));
+        const j = new iplib.IPv4Address("192.168.0.1");
+        assert.equal(j.getDec(), 3232235521);
+        const k = new iplib.IPv4Address(j.getDec(), iplib.IP_DEC);
+        assert.ok(j.equals(k));
     });
 });
